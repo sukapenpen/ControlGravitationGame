334539542b85df440f42c2e0ff58e55846ebe74a
sukapenpen <ne280194@senshu-u.jp>
2019-03-13 13:00:55 +0900

    追従するが回転はしないカメラ

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    private GameObject spaceship;
+    private Vector3 initPos;
+    
+    private void Start()
+    {
+        spaceship = GameObject.FindGameObjectWithTag("Spaceship");
+        initPos = this.transform.position;
+    }
+
+    private void Update()
+    {
+        var pos = spaceship.transform.position;
+        pos.z = initPos.z;
+        this.transform.position = pos;
+    }
+}
